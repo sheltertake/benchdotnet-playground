38e9fa0ea50ffc1f4f7a06ee2ac1a219c80b68af
Massimiliano Balestrieri <Massimiliano.Balestrieri@deltatre.com>
2019-07-01 13:40:49 +0200

    wip

---
+++ src/BenchJob/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Running;
+
+namespace MyBenchmarks
+{
+    
+    //[SimpleJob(
+    //    RunStrategy.Throughput,
+        
+    //    id:"MyJob",
+    //    invocationCount:1,
+    //    launchCount:100,
+    //    warmupCount:10,
+    //    targetCount: 15 //IterationCount -> MinIterationCount 15 MaxIterationCount 100
+
+    //)]
+    //[CoreJob]
+    public class HelloWorld
+    {
+        public static int Counter = 0;
+        [Params("HelloWorld")]
+        public string Param { get; set; }
+
+        [Benchmark]
+        public string Concat()
+        {
+            Console.WriteLine(Param + " " + ++Counter);
+            return Param;
+        }
+        
+    }
+    public class Program {
+        public static void Main (string[] args) {
+            var summary = BenchmarkRunner.Run<HelloWorld> ();
+        }
+    }
+}
---
+++ src/Benchmarks/Benchmarks/FileOperations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Benchmarks.Benchamarks
+{
+    public static class FileOperations
+    {
+        public static void BadWriteTempFile()
+        {
+            byte[] data = new byte[64 * 1024 * 1024];
+            var stopwatch = Stopwatch.StartNew();
+            var fileName = Path.GetTempFileName();
+            File.WriteAllBytes(fileName, data);
+            File.Delete(fileName);
+            stopwatch.Stop();
+            Console.WriteLine(nameof(BadWriteTempFile) + " " + stopwatch.ElapsedMilliseconds);
+        }
+
+        public static void BetterWriteTempFile()
+        {
+            int N = 10;//1000
+            byte[] data = new byte[64 * 1024 * 1024];
+            var measurements = new long[N];
+            for (int i = 0; i < N; i++)
+            {
+                var stopwatch = Stopwatch.StartNew();
+                var fileName = Path.GetTempFileName();
+                File.WriteAllBytes(fileName, data);
+                File.Delete(fileName);
+                stopwatch.Stop();
+                measurements[i] = stopwatch.ElapsedMilliseconds;
+                Console.WriteLine(nameof(BetterWriteTempFile) + " " + measurements[i]);
+            }
+            Console.WriteLine("Minimum = " + measurements.Min());
+            Console.WriteLine("Maximum = " + measurements.Max());
+            Console.WriteLine("Average = " + measurements.Average());
+        }
+    }
+}
