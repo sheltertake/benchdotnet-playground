1b94c7150e2df1e31db5191b21a6c5a7231a86ea
sheltertake <sheltertake@gmail.com>
2019-07-13 10:24:21 +0200

    wip

---
+++ 01-benchmark-basic/src/Benchmarks/Benchmarks.bat
@@ -0,0 +1,11 @@
+dotnet run -c Release 1
+dotnet run -c Release 2
+dotnet run -c Release 3
+dotnet run -c Release 4
+dotnet run -c Release 5
+dotnet run -c Release 6
+dotnet run -c Release 7
+dotnet run -c Release 8
+dotnet run -c Release 9
+dotnet run -c Release 10
+dotnet run -c Release 11
