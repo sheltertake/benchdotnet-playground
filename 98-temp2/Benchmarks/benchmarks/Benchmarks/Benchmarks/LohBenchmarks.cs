bfc2bb9fe0d68b6482896d81f46dba7158d950d1
sheltertake <sheltertake@gmail.com>
2019-07-13 18:26:39 +0200

    loh

---
+++ 98-temp2/Benchmarks/benchmarks/Benchmarks/Benchmarks/LohBenchmarks.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class LohBenchmarks
+    {
+        [Benchmark]
+        public byte[] Allocate84900()
+        {
+            return new byte[84900];
+        }
+        [Benchmark]
+        public byte[] Allocate85000()
+        {
+            return new byte[85000];
+        }
+    }
+}
