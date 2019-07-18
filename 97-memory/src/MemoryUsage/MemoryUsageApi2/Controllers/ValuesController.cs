af481838faeefcf23a0812d8f8d5696452fd2c54
sheltertake <sheltertake@gmail.com>
2019-07-18 23:17:31 +0200

    web2

--- 97-memory/src/MemoryUsage/MemoryUsageApi2/Controllers/ValuesController.cs
+++ 97-memory/src/MemoryUsage/MemoryUsageApi2/Controllers/ValuesController.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace MemoryUsageApi.Controllers
+namespace MemoryUsageApi2.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
