017b353edcf06aea643732c4cef00eabcbb3be07
Massimiliano Balestrieri <Massimiliano.Balestrieri@deltatre.com>
2019-09-24 11:37:13 +0200

    dotnetcore3

---
+++ 94-webapi-througput/webapi2/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi2.Controllers
+{
+    [Route("helloworld")]
+    public class HelloWorldController
+    {
+        // GET api/values
+        [HttpGet]
+        public int Get()
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ 94-webapi-througput/webapi3/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace webapi3.Controllers
+{
+   [Route("helloworld")]
+    public class HelloWorldController
+    {
+        // GET api/values
+        [HttpGet]
+        public int Get()
+        {
+            return 0;
+        }
+    }
+}
