7f3fccfc749eb95ac11fcf6d9f6e4692ea884b10
Massimiliano Balestrieri <Massimiliano.Balestrieri@deltatre.com>
2019-09-25 09:47:29 +0200

    database and structs

---
+++ 92-orm/ConsoleApp/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using ServiceStack.Data;
+using ServiceStack.DataAnnotations;
+using ServiceStack.OrmLite;
+using ServiceStack.OrmLite.SqlServer;
+
+namespace ConsoleApp
+{
+    [ShortRunJob]
+    [MemoryDiagnoser]
+    public class Benchmarks
+    {
+        //private IDbConnection DapperConnection;
+        private IDbConnectionFactory OrmLiteConnectionFactory;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var connectionString = "Server=LI-598;Database=test;User Id=sa;Password=YourStrong@Passw0rd;";
+            OrmLiteConnectionFactory = new OrmLiteConnectionFactory(connectionString, SqlServerOrmLiteDialectProvider.Instance);
+            //DapperConnection = new SqlConnection(connectionString);
+            //DapperConnection.Open();
+            //using (var connection = OrmLiteConnectionFactory.Open())
+            //{
+            //    connection.DeleteAll<Todo>();
+            //    for (int i = 0; i < 1_000; i++)
+            //    {
+            //        connection.InsertAll<Todo>(Enumerable.Repeat<Todo>(new Todo()
+            //        {
+            //            Label = Guid.NewGuid().ToString()
+            //        }, 1_000));
+            //    }
+                
+            //}
+        }
+
+
+        [Benchmark]
+        public int Dapper()
+        {
+            using (var connection = OrmLiteConnectionFactory.Open())
+            {
+                var ret = connection.Query<Todo>("select * from todo").AsList();
+                return ret.Count;
+            }
+        }
+        [Benchmark]
+        public bool DapperStruct()
+        {
+            using (var connection = OrmLiteConnectionFactory.Open())
+            {
+                var ret = connection.Query<Struct>("select * from todo").AsEnumerable();
+                return ret.Any();
+            }
+        }
+        [Benchmark]
+        public int DapperContrib()
+        {
+            using (var connection = OrmLiteConnectionFactory.Open())
+            {
+                var ret = connection.GetAll<Todo>().AsList();
+                return ret.Count;
+            }
+        }
+        
+
+        [Benchmark]
+        public int OrmLite()
+        {
+            using (var connection = OrmLiteConnectionFactory.Open())
+            {
+                var ret = connection.Select<Todo>().AsList();
+                return ret.Count;
+            }
+        }
+        //[Benchmark]
+        //public int OrmLiteStruct()
+        //{
+        //    using (var connection = OrmLiteConnectionFactory.Open())
+        //    {
+        //        var ret = connection.Select<Struct>().AsList();
+        //        return ret.Count;
+        //    }
+        //}
+        [Table("Todo")]
+        class Todo
+        {
+            [AutoIncrement]
+            public int Id { get; set; }
+            public string Label { get; set; }
+        }
+
+        struct Struct
+        {
+            public int Id;
+            public string Label;
+        }
+        
+    }
+
+    class Program
+    {
+        static void Main() => BenchmarkRunner.Run<Benchmarks>();
+    }
+}
---
+++ 93-method-response/ConsoleApp/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace ConsoleApp
+{
+    [ShortRunJob]
+    [MemoryDiagnoser]
+    [DisassemblyDiagnoser]
+    public class Benchmark
+    {
+
+        private const int N = 1_000_000;
+
+        [Benchmark]
+        public int ClassP()
+        {
+            var responses = new List<ClassWithProps>(N);
+            for (int i = 0; i < N; i++)
+            {
+                responses.Add(CalcReturnClassWithProps());
+            }
+            return responses.Count;
+        }
+        [Benchmark]
+        public int ClassF()
+        {
+            var responses = new List<ClassWithFields>(N);
+            for (int i = 0; i < N; i++)
+            {
+                responses.Add(CalcReturnClassWithFields());
+            }
+            return responses.Count;
+        }
+        [Benchmark]
+        public int StructP()
+        {
+            var responses = new List<StructWithProps>(N);
+            for (int i = 0; i < N; i++)
+            {
+                var calcReturnStructWithProps = CalcReturnStructWithProps();
+                responses.Add(calcReturnStructWithProps);
+            }
+            return responses.Count;
+        }
+        [Benchmark]
+        public int StructF()
+        {
+            var responses = new List<StructWithFields>(N);
+            for (int i = 0; i < N; i++)
+            {
+                responses.Add(CalcReturnStructWithFields());
+            }
+            return responses.Count;
+        }
+        [Benchmark]
+        public int Tuple()
+        {
+            var responses = new List<Tuple<int, string>>(N);
+            for (int i = 0; i < N; i++)
+            {
+                responses.Add(CalcReturnTuple());
+            }
+            return responses.Count;
+        }
+        [Benchmark]
+        public int Integer()
+        {
+            var responses = new List<int>(N);
+            for (int i = 0; i < N; i++)
+            {
+                responses.Add(CalcReturnInt());
+            }
+            return responses.Count;
+        }
+        public ClassWithProps CalcReturnClassWithProps()
+        {
+            return new ClassWithProps()
+            {
+                Code = 1,
+                Message = "Error Message"
+            };
+        }
+        public ClassWithFields CalcReturnClassWithFields()
+        {
+            return new ClassWithFields()
+            {
+                Code = 1,
+                Message = "Error Message"
+            };
+        }
+        public StructWithProps CalcReturnStructWithProps()
+        {
+            return new StructWithProps()
+            {
+                Code = 1,
+                Message = "Error Message"
+            };
+        }
+        public StructWithFields CalcReturnStructWithFields()
+        {
+            return new StructWithFields()
+            {
+                Code = 1,
+                Message = "Error Message"
+            };
+        }
+        public Tuple<int,string> CalcReturnTuple()
+        {
+            return new Tuple<int, string>(1, "ErrorMessage");
+        }
+        public int CalcReturnInt()
+        {
+            return 1;
+        }
+        public class ClassWithProps
+        {
+            public int Code { get; set; }
+            public string Message { get; set; }
+        }
+        public class ClassWithFields
+        {
+            public int Code;
+            public string Message;
+        }
+
+        public struct StructWithFields
+        {
+            public int Code;
+            public string Message;
+        }
+
+        public struct StructWithProps
+        {
+            public int Code { get; set; }
+            public string Message { get; set; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+}
